385b5081905137a76cc4ccc188720c8cfa082fbf
aghl56 <73744002+aghl56@users.noreply.github.com>
2020-11-05 13:52:58 +0300

    lab3

--- src/main/java/ru/mtuci/simpleproject/CommandLineAppStartupRunner.java
+++ src/main/java/ru/mtuci/simpleproject/CommandLineAppStartupRunner.java
@@ -17,6 +17,6 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 
     @Override
     public void run(String...args) throws Exception{
-        System.out.println(filmsRepository.findById(1L).get());
+        //System.out.println(filmsRepository.findById(1L).get());
     }
 }
